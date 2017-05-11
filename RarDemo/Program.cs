38db291241c0f1e04e81182068ee7e5a86583576
sqb <shenqiangbin@163.com>
2017-05-11 15:34:21 +0800

    示例完成

--- RarDemo/Program.cs
+++ RarDemo/Program.cs
@@ -9,8 +9,16 @@ namespace RarDemo
     {
         static void Main(string[] args)
         {
+            //解压
             //RarHelper.Unpack("d:/demo.rar", "d:/testRar/");
-            RarHelper.Unpack("d:/demo.zip", "d:/testRar/");            
+            RarHelper.Unpack("d:/demo.zip", "d:/testRar/");
+
+            //压缩
+            List<string> files = new List<string>() { "d:/demo.xls", "d:/demo2.xlsx" };
+            RarHelper.Pack(files, "d:/abc.zip");
+
+            //压缩
+            RarHelper.Pack("d:/testRar", "d:/t.zip");
         }
     }
 }
